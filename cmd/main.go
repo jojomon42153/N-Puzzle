1351c9dff0f884f5143522d1c7c011c6768413ff
Shankithan Thevakumar <shthevak@student.le-101.fr>
2020-02-25 19:07:31 +0100

    Starting to create a valid 'taquin'

--- cmd/main.go
+++ cmd/main.go
@@ -3,5 +3,68 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("hello world")
+
+	size := 5
+	tab := make([][]int, size)
+	for i := 0; i < size; i++ {
+		tab[i] = make([]int, size);
+	}
+
+    count := 1;
+    i := 0;
+    j := 0;
+
+    for count < size * size {
+        for j < size {
+            if (tab[i][j] == 0) {
+                tab[i][j] = count;
+                count++;
+            }
+            if (j + 1 >= size || ( j + 1 < size && tab[i][j+1] != 0)) {
+                break
+            }
+            j++;
+        }
+        for i < size {
+            if (tab[i][j] == 0) {
+                tab[i][j] = count;
+                count++;
+            }
+            if (i + 1 >= size || ( i + 1 < size && tab[i + 1][j] != 0)) {
+                break
+            }
+            i++;
+        }
+        for j >= 0 {
+            if (tab[i][j] == 0) {
+                tab[i][j] = count;
+                count++;
+            }
+            if (j - 1 < 0 || ( j - 1 >= 0 && tab[i][j - 1] != 0)) {
+                break
+            }
+            j--;
+        }
+        for i >= 0 {
+            if (tab[i][j] == 0) {
+                tab[i][j] = count;
+                count++;
+            }
+            if (i - 1 < 0 || ( i - 1 >= 0 && tab[i-1][j] != 0)) {
+                break
+            }
+            i--;
+        }
+    }
+
+    k := 0
+    for k < size{
+        l := 0
+        for l < size {
+            fmt.Printf("%3d", tab[k][l])
+            l++
+        }
+        k++
+        fmt.Printf("\n")
+    }
 }
