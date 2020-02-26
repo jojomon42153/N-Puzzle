47571ce445f3174aab1aef39b9ea6e4e4ea42a03
Johann Monnerie <jojomon42153@gmail.com>
2020-02-26 15:17:51 +0100

    [jmonneri] main: add dataset

--- cmd/main.go
+++ cmd/main.go
@@ -6,15 +6,50 @@
 /*   By: jmonneri <jmonneri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/26 13:48:27 by jmonneri          #+#    #+#             */
-/*   Updated: 2020/02/26 13:54:34 by jmonneri         ###   ########.fr       */
+/*   Updated: 2020/02/26 15:16:54 by jmonneri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 package main
 
-import "fmt"
+func createDataSet(n int) {
+	var initial *state = &state{
+		parent:        nil,
+		state2D:       make([][]int, n),
+		state1D:       make([]int, n*n),
+		initialCost:   0,
+		heuristicCost: 0,
+		totalCost:     0,
+		isOpen:        true,
+	}
+	// init state 2D
+	//35 34 33 32 31 30
+	//29 28 27 26 25 24
+	//23 22 21 20 19 18
+	//17 16 15 14 13 12
+	//11 10 9  8  7  6
+	//5  4  3  2  1  0
+	for i := 0; i < n; i++ {
+		initial.state2D[i] = make([]int, n)
+	}
+	var i int = n*n - 1
+	for y, line := range initial.state2D {
+		for x := range line {
+			initial.state2D[y][x] = i
+			i--
+		}
+	}
+	// init state1D
+	for _, line := range initial.state2D {
+		initial.state1D = append(initial.state1D, line...)
+	}
+	// init heuristicCost
+
+}
 
 func main() {
-	initEnv(6)
-	fmt.Println(env)
+	n := 6
+	initEnv(n)
+
+	createDataSet(n)
 }
